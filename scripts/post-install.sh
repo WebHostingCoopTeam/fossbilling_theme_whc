be4bf01cc1d638e44a61a2410806797c1c41d0f8
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-01-29 10:18:32 -0600

    www-data

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -6,6 +6,7 @@ docker exec fossbilling_theme_whc-fossbilling-1 rm -rfv /var/www/html/install
 set -e
 docker exec fossbilling_theme_whc-fossbilling-1 ls /var/www/html
 docker exec fossbilling_theme_whc-fossbilling-1 chmod 644 /var/www/html/config.php
+docker exec fossbilling_theme_whc-fossbilling-1 chown -R www-data: /var/www/html/themes
 docker exec fossbilling_theme_whc-fossbilling-1 ls -alh /var/www/html/config.php
 
 set +x 
