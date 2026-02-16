e1a2db0276762bed708d9aaee0948265d214b69b
Thoth <josh@webhosting.coop>
2026-02-16 16:11:31 -0600

    typo

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -8,8 +8,8 @@ docker exec fossbilling_theme_whc-fossbilling-1 ls /var/www/html
 docker exec fossbilling_theme_whc-fossbilling-1 chmod 644 /var/www/html/config.php
 docker exec fossbilling_theme_whc-fossbilling-1 chown -R www-data: /var/www/html/themes
 docker exec fossbilling_theme_whc-fossbilling-1 ls -alh /var/www/html/config.php
-chmod 777 submdodules/fb_whc/assets
-chmod 777 submdodules/fb_whc_admin/assets
+chmod 777 submodules/fb_whc/assets
+chmod 777 submodules/fb_whc_admin/assets
 
 set +x 
 echo 'login with your admin creds at: http://localhost:5080/admin'
